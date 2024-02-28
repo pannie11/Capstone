0b841c6cbc7dd2a5ef9c571c6973cee40ee3e9ed
Annie Pan <anniepan@Annies-MBP.fios-router.home>
2024-02-27 20:38:14 -0500

    sort results endpoint complete

--- src/Components/AllCarts.jsx
+++ src/Components/AllCarts.jsx
@@ -6,6 +6,7 @@ export default function AllCarts() {
     const [carts, setCarts] = useState([])
     const navigate = useNavigate()
     const [limit, setLimit] = useState(null)
+    const [sortValue, setSortValue] = useState('')
 
     useEffect(() => {
         async function getCarts() {
@@ -43,28 +44,49 @@ export default function AllCarts() {
             }
         }
 
+    async function sort(e) {
+        e.preventDefault()
+
+        try {
+            const response = await fetch(`${api}/carts?sort=${sortValue}`)
+            const result = await response.json()
+
+            setCarts(result)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
     return (
         <>
-        <form onSubmit={limitResults}>
-            <label>Limit results: </label>
-            <input type="number" onChange={(e) => setLimit(e.target.value)} value={limit} />
-            <button>Submit</button>
-        </form>
-        {carts.map((cart) => {
-            return (
-                <div key={cart.id}>
-                    <button onClick={() => navigate(`/carts/${cart.id}`)}>User Id: {cart.userId}  &ensp;|&ensp; Cart Id: {cart.id}</button>
-                    <p>Date: {cart.date}</p>
-                   {cart.products.map((product) => {
-                    return (
-                        <div key={product.productId}>
-                        <p>Product id: {product.productId} &ensp;|&ensp; Product quantity: {product.quantity}</p>
-                        </div>
-                    )
-                   })}
-                </div>
-            )
-        })}
+            <form onSubmit={limitResults}>
+                <label>Limit results: </label>
+                <input type="number" onChange={(e) => setLimit(e.target.value)} value={limit} />
+                <button>Submit</button>
+            </form>
+            <form onSubmit={sort}>
+                <label>Sort by: </label>
+                <select onChange={(e) => setSortValue(e.target.value)} value={sortValue}>
+                    <option value="asc">Ascending</option>
+                    <option value="desc">Descending</option>
+                </select>
+                <button>Sort</button>
+            </form>
+            {carts.map((cart) => {
+                return (
+                    <div key={cart.id}>
+                        <button onClick={() => navigate(`/carts/${cart.id}`)}>User Id: {cart.userId}  &ensp;|&ensp; Cart Id: {cart.id}</button>
+                        <p>Date: {cart.date}</p>
+                    {cart.products.map((product) => {
+                        return (
+                            <div key={product.productId}>
+                            <p>Product id: {product.productId} &ensp;|&ensp; Product quantity: {product.quantity}</p>
+                            </div>
+                        )
+                    })}
+                    </div>
+                )
+            })}
         </>
     )
 }
